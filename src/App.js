c676ac36f679247e08d0cdfbd42a930cd703e641
bringmethecode <dev@ansky.sh>
2019-03-09 23:25:18 -0600

    Update app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
+import FontFaceObserver from 'fontfaceobserver'
 import './app.css'
 
+const inconsolataFont = new FontFaceObserver('Inconsolata')
 const { ipcRenderer } = window.require('electron')
 
 class App extends Component {
   state = {
     value: '',
+    loading: true,
   }
 
   componentDidMount = async () => {
+    await inconsolataFont.load()
+    this.setState({ loading: false })
     ipcRenderer.send('get-note')
     ipcRenderer.on('note', (e, note) => this.setState({ value: note }))
     await ipcRenderer.on('ready-close', () => {
@@ -22,12 +27,23 @@ class App extends Component {
   render() {
     return (
       <>
-        <span>#</span>
-        <input
-          placeholder='Write a new note...'
-          value={this.state.value}
-          onChange={e => this.setState({ value: e.target.value })}
-        />
+      {
+        this.state.loading ? (
+          <div className='loader' />
+        ) : (
+          <>
+            <h1 className='title'>Note:</h1>
+            <div className='note'>
+              <span>#</span>
+              <textarea
+                placeholder='Write a new note...'
+                value={this.state.value}
+                onChange={e => this.setState({ value: e.target.value })}
+              />
+            </div>
+          </>
+        )
+      }
       </>
     )
   }
