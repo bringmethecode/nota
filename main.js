5b8b72a194ab95c837fe50f545eec24d8bdb8896
Andre Aldana <dev@ansky.sh>
2019-03-09 23:25:59 -0600

    Add utils to main file

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { app, BrowserWindow, ipcMain } = require('electron')
+const {
+  app, BrowserWindow, ipcMain, Menu,
+} = require('electron')
 const Store = require('electron-store')
+require('dotenv').config()
 const {
   getWindowState, setWindowState, getNote, saveNote,
 } = require('./storeHelpers')
+const macMenu = require('./src/utils/mac/macMenu')
+const macAbout = require('./src/utils/mac/macAbout')
 
 const store = new Store({ name: 'nota' })
 let mainWindow
-let a = true
+let notReady = true
 const windowState = getWindowState(store)
 const storedNote = getNote(store)
 
 const createWindow = () => {
+  Menu.setApplicationMenu(Menu.buildFromTemplate(macMenu(store)))
+  macAbout()
   mainWindow = new BrowserWindow({
     x: windowState.x || 0,
     y: windowState.y || 0,
-    width: windowState.width || 500,
-    height: windowState.height || 100,
+    width: windowState.width || 400,
+    height: windowState.height || 150,
     backgroundColor: '#050505',
     alwaysOnTop: true,
     title: 'nota',
@@ -28,16 +35,19 @@ const createWindow = () => {
   const { webContents } = mainWindow
   mainWindow.loadFile('index.html')
   mainWindow.once('show', () => webContents.send('note', storedNote))
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', () => {
     mainWindow = null
   })
 
   mainWindow.on('close', (e) => {
-    a = !a
-    webContents.send('ready-close')
-    if (a) e.preventDefault()
+    if (notReady) {
+      const bounds = mainWindow.getBounds()
+      setWindowState(store, bounds)
+      webContents.send('ready-close')
+      e.preventDefault()
+    }
   })
 
   ipcMain.on('get-note', () => {
@@ -49,9 +59,12 @@ const createWindow = () => {
   })
 
   ipcMain.on('closed', () => {
+    /* this ends the vicious loop we all get trapped into */
+    notReady = false
     app.quit()
   })
 }
+
 
 app.on('ready', createWindow)
 
