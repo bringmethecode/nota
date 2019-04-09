b4cbffa1aaec9b6ee7466cc6999eeb9b340a9ca7
bringmethecode <dev@anksy.sh>
2019-04-08 20:20:20 -0600

    Update React app structure

---
+++ src/containers/App.js
@@ -0,0 +1,66 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader/root'
+import FontFaceObserver from 'fontfaceobserver'
+import TextareaAutosize from 'react-autosize-textarea'
+import './style.scss'
+
+const inconsolataFont = new FontFaceObserver('Inconsolata')
+const { ipcRenderer } = window.require('electron')
+
+class App extends Component {
+  state = {
+    value: '',
+    loading: true,
+  }
+
+  componentDidMount = async () => {
+    await inconsolataFont.load()
+    ipcRenderer.send('get-note')
+    ipcRenderer.on('retrieve-note', (e, note) => {
+      this.setState({ loading: false, value: note })
+    })
+    ipcRenderer.on('ready-to-close', () => {
+      ipcRenderer.send('close-app')
+    })
+  }
+
+  updateValue = value => this.setState({ value })
+
+  resizeWindow = (width, height) => {
+    ipcRenderer.send('resize-area', { width, height })
+  }
+
+  closeWindow = () => {
+    ipcRenderer.send('prepare-close', this.state.value)
+    this.setState({ loading: true })
+  }
+
+  render() {
+    return (
+      <>
+        {
+          this.state.loading ? (
+            <div className='loader' />
+          ) : (
+            <div className='app'>
+              <span className='close-btn' onClick={this.closeWindow} />
+              <span className='hash'>#</span>
+              <TextareaAutosize
+                placeholder='new reminder ✏️'
+                value={this.state.value}
+                onChange={e => this.updateValue(e.target.value)}
+                maxRows={20}
+                onResize={({
+                  srcElement: { clientWidth, clientHeight }
+                }) => this.resizeWindow(clientWidth, clientHeight)}
+              />
+            </div>
+          )
+        }
+      </>
+    )
+  }
+}
+
+export default hot(App)
